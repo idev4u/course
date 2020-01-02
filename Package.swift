3e9c4a6db1c29a169e9c567d05b7589cbd51ca52
Norman Sutorius <us.idev.student@googlemail.com>
2020-01-02 14:19:30 +0100

    Add postgresql driver
    # 2

--- Package.swift
+++ Package.swift
@@ -9,9 +9,11 @@ let package = Package(
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        // Fluent
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "1.0.0" ),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
+        .target(name: "App", dependencies: ["Leaf", "Vapor", "FluentPostgreSQL"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
