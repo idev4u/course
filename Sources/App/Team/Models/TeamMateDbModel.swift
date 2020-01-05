d8b13c062a18c48ecc873202e8224c6696022201
Norman Sutorius <us.idev.student@googlemail.com>
2020-01-05 19:54:02 +0100

    Remove unused code
    #14

--- Sources/App/Team/Models/TeamMateDbModel.swift
+++ Sources/App/Team/Models/TeamMateDbModel.swift
@@ -8,29 +8,6 @@
 import FluentPostgreSQL
 import Vapor
 
-//final class TeamMateDbModel: PostgreSQLModel {
-//    /// The unique identifier for this user.
-//    var id: Int?
-//    /// The user's full name.
-//    var name: String
-//    var surename: String
-//    var image: File?
-//    var isOut: Bool?
-//    var assingedTrackId: Int?
-//
-//    /// Creates a new teammate.
-//    init(id: Int? = nil, name: String, surename: String, image: File, isOut: Bool, assingedTrackId: Int) {
-//        self.id = id
-//        self.name = name
-//        self.surename = surename
-//        self.image = image
-//        self.isOut = isOut
-//        self.assingedTrackId = assingedTrackId
-//    }
-//}
-//
-//extension TeamMateDbModel: Content { }
-
 struct TeamMateDbModel: PostgreSQLModel, Content, Migration, Parameter {
     var id: Int?
     var name: String
@@ -39,4 +16,3 @@ struct TeamMateDbModel: PostgreSQLModel, Content, Migration, Parameter {
     var isOut: Bool?
     var assignedTrackId: Int?
 }
-
