3be882514c32e3246da94141bd4406423d130f97
Norman Sutorius <us.idev.student@googlemail.com>
2020-04-29 09:25:35 +0200

    Disable compression cause there seems to be a issue with heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -40,7 +40,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: ParkingLotTopic.self, database: .psql)
     services.register(migrations)
     
-    // Extend the body size of Swift NIO from 1Mb to 7Mb
-    services.register(NIOServerConfig.default(maxBodySize: 7_000_000, supportCompression: true))
+    // Extend the body size of Swift NIO from 1Mb to 4Mb
+//    services.register(NIOServerConfig.default(maxBodySize: 4_000_000, supportCompression: true))
+    services.register(NIOServerConfig.default(maxBodySize: 4_000_000, supportCompression: false))
                                                             
 }
